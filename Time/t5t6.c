e705a8ff033ab1132df6d307a3ddd5b94c05e59f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-25 23:07:11 +0800

    Update t5t6.c

--- Time/t5t6.c
+++ Time/t5t6.c
@@ -7,7 +7,7 @@ void function t5t6(){
        'k * asinh(www - t5) + l * acosh(www + t6)',
     ]
     void t6;[   
-       'I * asinh(NaN - t5) + J * acosh(NaN + t6)',
-       'K * asinh(0 + t5) + L * acosh(0 - t6)',
+       'I * asinh(NaN - t5)' + 'J * acosh(NaN + t6)',
+       'K * asinh(0 + t5)' + 'L * acosh(0 - t6)',
     ]
 }
