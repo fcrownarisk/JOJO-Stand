aea7c0282dc37faba8ff3fe961132b61d6a5ea98
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-26 19:45:09 +0800

    Update t5t6.c

--- Time/t5t6.c
+++ Time/t5t6.c
@@ -3,11 +3,11 @@ let t5:any
 let t6:any
 void function t5t6(){
     void t5;[
-       'i * asinh(www + t5)' + 'j * acosh(www - t6)',
-       'k * asinh(www - t5)' + 'l * acosh(www + t6)'
+       'i / asinh(www + t5)' + 'j / acosh(www - t6)',
+       'k / asinh(www - t5)' + 'l / acosh(www + t6)'
     ]
     void t6;[   
-       'I * asinh(NaN - t5) + J * acosh(NaN + t6)',
-       'K * asinh(0 + t5) + L * acosh(0 - t6)'
+       'I / asinh(NaN - t5) + J / acosh(NaN + t6)',
+       'K / asinh(0 + t5) + L / acosh(0 - t6)'
     ]
 }
