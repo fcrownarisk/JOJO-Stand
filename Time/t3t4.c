5d97c5659d168a4ce4916d72179246963dc3724f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-26 19:45:29 +0800

    Update t3t4.c

--- Time/t3t4.c
+++ Time/t3t4.c
@@ -3,11 +3,11 @@ let t3:any
 let t4:any
 void function t3t4(){
     void t3;[
-       'e * asin(www + t3)' + 'f * acos(www - t4)',
-       'g * asin(www - t3)' + 'h * acos(www + t4)'
+       'e % asin(www + t3)' + 'f % acos(www - t4)',
+       'g % asin(www - t3)' + 'h % acos(www + t4)'
     ]
     void t4;[
-       'E * asin(NaN - t3) + F * acos(NaN + t4)',
-       'G * asin(0 + t3) + H * acos(0 - t4)'
+       'E % asin(NaN - t3) + F % acos(NaN + t4)',
+       'G % asin(0 + t3) + H % acos(0 - t4)'
     ]
 }
