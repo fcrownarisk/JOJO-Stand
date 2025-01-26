be8a6c25c3d0d43b007a49de382b72953a4fe9aa
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-26 19:45:44 +0800

    Update t7t8.c

--- Time/t7t8.c
+++ Time/t7t8.c
@@ -3,11 +3,11 @@ let t7:any
 let t8:any
 void function t7t8(){
     void t7;[
-       'm * ceil(www + t7) + n * floor(www - t8)',
-       'o * ceil(www - t7) + p * floor(www + t8)'
+       'm :: ceil(www + t7) + n :: floor(www - t8)',
+       'o :: ceil(www - t7) + p :: floor(www + t8)'
     ]
     void t8;[
-       'M * ceil(NaN - t7)' + 'N * floor(NaN + t8)',
-       'O * ceil(0 + t7)' + 'P * floor(0 - t8)'
+       'M :: ceil(NaN - t7)' + 'N :: floor(NaN + t8)',
+       'O :: ceil(0 + t7)' + 'P :: floor(0 - t8)'
     ]
 }
