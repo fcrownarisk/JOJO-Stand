b56d18ff6fc90b64f6f4488daecc6fd6910200bc
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-26 19:46:09 +0800

    Update t9t10.c

--- Time/t9t10.c
+++ Time/t9t10.c
@@ -3,11 +3,11 @@ let t9:any
 let t10:any
 void function t9t10(){
     void t9;[
-       'o * exp(www + t9) + p * log(www - t10)',
-       'q * exp(www - t9) + r * log(www + t10)'
+       'o ^ exp(www + t9) + p ^ log(www - t10)',
+       'q ^ exp(www - t9) + r ^ log(www + t10)'
     ]
     void t10;[
-       'O * exp(NaN - t9)' + 'P * log(NaN + t10)',
-       'Q * exp(0 + t9)' + 'R * log(0 - t10)'
+       'O ^ exp(NaN - t9)' + 'P ^ log(NaN + t10)',
+       'Q ^ exp(0 + t9)' + 'R ^ log(0 - t10)'
     ]
 }
