8c3e749e18d6d9195f6736b928037a1b349e7c28
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-02-14 16:50:29 +0800

    Update t7t8.c

--- Time/t7t8.c
+++ Time/t7t8.c
@@ -1,7 +1,6 @@
 #include "HTML/GoldenWind.html"
-let t7:any
-let t8:any
 void function t7t8(){
+    int t7,t8;
     void t7;[
        'm :: ceil(www + t7) + n :: floor(www - t8)',
        'o :: ceil(www - t7) + p :: floor(www + t8)'
